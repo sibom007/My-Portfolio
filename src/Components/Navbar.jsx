e89c29ea60d5c3ca7fc2dbaab5918ba63506f670
sibom007 <vaispark400@gmail.com>
2024-03-21 10:12:01 +0600

    all add

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,16 @@
-
+import { AiOutlineCloudDownload } from "react-icons/ai";
 
 const Navbar = () => {
 
     return (
         <div className="w-full fixed z-[999] px-20 py-3 flex justify-between items-center backdrop-filter backdrop-blur-sm">
             <div><img className="size-5 md:size-[50px]" src="../../public/sibom-logo.png" alt="" /></div>
-            <div className="text-[2vw] md:text-base font-medium text-white flex gap-2 md:gap-10">{["Home", "Servise", "Our Work", "About Us", "Contact Us"].map((i, index) => (<a className="animate-bounce text-blue-300" key={index}>{i}</a>))}</div>
+            <div className="text-[2vw] md:text-base font-medium text-white flex gap-2 md:gap-10">{["Home", "Servise", "Our Work", "About Us", "Contact Us"].map((i, index) => (<a className="animate-bounce text-blue-300" key={index}>{i}
+            </a>))}</div>
+            <a
+                href={"../../public/Reme 1.pdf.pdf"}
+                download={"Resume"}
+                className="ml-5 bg-[#f1eaed] p-2 rounded-lg uppercase flex items-center border-2 shadow-md hover:bg-[#ebdfe4] duration-200">My resume <AiOutlineCloudDownload className="size-5 ml-3" /> </a>
         </div>
     );
 };
--- src/Components/PlayPage.jsx
+++ src/Components/PlayPage.jsx
@@ -21,7 +21,7 @@ const PlayPage = () => {
     return (
         <div data-scroll data-scroll-speed='-.2' className=" h-screen overflow-hidden w-full px-32 py-14 pt-0">
             <h1 className="underline uppercase text-4xl text-center mb-5 font-semibold">Play With me</h1>
-            <div style={{ backgroundImage: 'url(../../public/eye2.png)' }} className='w-full rounded-lg relative h-full'>
+            <div style={{ backgroundImage: 'url(../assets/eye2.png)' }} className='w-full rounded-lg relative h-full'>
                 <div className="absolute flex gap-8 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]">
                     <div className="w-[15vw] h-[15vw] flex items-center justify-center rounded-full bg-zinc-100">
                         <div className={`w-2/3 h-2/3 relative rounded-full bg-zinc-900 `}>
